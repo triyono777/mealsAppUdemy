3e4738d17039c64fe96b2adca865cb13b5852e61
triyono <yono.tri@gmail.com>
2020-03-22 23:10:36 +0700

    tambah category item

---
+++ lib/dummy_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(id: 'c1', title: 'Italian', color: Colors.purple),
+  Category(id: 'c2', title: 'Indonesia', color: Colors.blue),
+  Category(id: 'c3', title: 'Arab', color: Colors.deepPurple),
+  Category(id: 'c4', title: 'American', color: Colors.orange),
+  Category(id: 'c5', title: 'Jawa', color: Colors.yellow),
+];
