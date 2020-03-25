d9c072e271a1abd57a282cf0d946b9f707ef2058
triyono <yono.tri@gmail.com>
2020-03-25 17:50:15 +0700

    parsing data to meal screen

--- lib/screen/category_meals_screen.dart
+++ lib/screen/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/dummy_data.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -21,7 +22,13 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(
+            title: categoryMeals[index].title,
+            duration: categoryMeals[index].duration,
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
+            imageUrl: categoryMeals[index].imageUrl,
+          );
         },
         itemCount: categoryMeals.length,
       ),
