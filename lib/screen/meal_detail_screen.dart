92bdeb0b56692ebb795b7fc38c9a2b5ee97ca04b
triyono <yono.tri@gmail.com>
2020-03-26 21:43:34 +0700

    add firstWhere list

--- lib/screen/meal_detail_screen.dart
+++ lib/screen/meal_detail_screen.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
   @override
   Widget build(BuildContext context) {
     final mealID = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealID);
     return Scaffold(
       appBar: AppBar(
-        title: Text('$mealID'),
+        title: Text(selectedMeal.title),
       ),
-      body: Center(
-        child: Text('The meal! $mealID'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
       ),
     );
   }
