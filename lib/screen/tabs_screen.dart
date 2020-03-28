70c54a71b8adb41ac3a7a96c945dccd5f72f9ece
triyono <yono.tri@gmail.com>
2020-03-28 21:59:02 +0700

    add bottom bar

--- lib/screen/tabs_screen.dart
+++ lib/screen/tabs_screen.dart
@@ -10,32 +10,19 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-//      initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: 'Favorite',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
       ),
+      body: null,
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.category), title: Text('Categories')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.star), title: Text('Favorites')),
+          ]),
     );
   }
 }
